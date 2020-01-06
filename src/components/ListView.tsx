9dfd6ca8a573f826182db583e6bd6552cb25105a
Enrique Chong <echong112@gmail.com>
2020-01-06 18:52:50 -0500

    linking up state

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ListItem from './ListItem';
+import { useSelector } from 'react-redux';
 
 interface Props {
   list: any[];
@@ -7,7 +8,18 @@ interface Props {
   activeIndex?: number;
 };
 
+
 const ListView: React.FC<Props> = (props) => {
+  const isUp = useSelector((state: any) => {
+    console.log(state.isUp);
+    if (state.isUp) {
+      console.log('isup');
+    } else {
+      console.log('isDown');
+    }
+    return state.isUp;
+  });
+  
   return (
     <div>
       {props.list && props.list.map((item, k) => (
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,13 @@
 import { combineReducers } from 'redux'
 
-const isUp = (state, action) => {
-  console.log(action);
+const isUp = (state = false, action) => {
   switch(action.type) {
     case 'IS_UP':
       return action.isUp;
     case 'IS_DOWN': 
       return action.isUp;
     default: 
-      return isUp;
+      return state;
   }
 }
 
