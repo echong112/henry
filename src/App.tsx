53bffc2e9abf39fbb6927f15f4236906a9f9a08d
Enrique Chong <echong112@gmail.com>
2020-01-06 17:28:44 -0500

    updating name for main

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Index from './routes/Index';
+import Home from './routes/Home';
 import Portfolio from './routes/Portfolio';
 import Controls from './components/Controls';
 
@@ -14,7 +14,7 @@ const App: React.FC = () => {
           <div className="bezel">
             <div className="screen">
               <Switch>
-                <Route path="/" exact component={Index} />
+                <Route path="/" exact component={Home} />
                 <Route path="/experience/:slug" component={Portfolio} />
                 <Route path="/experience/" component={Portfolio} />
                 <Route path="/education/:slug" component={Portfolio} />
