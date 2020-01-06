3b4bffb90ecb3e51809f74d0ffd129cdf38d6be6
Enrique Chong <echong112@gmail.com>
2020-01-06 10:32:08 -0500

    boxes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Index from './Index';
 import Product from './Product';
+import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Index} />
-        <Route path="/item" component={Product} />
-      </Switch>
-      <div className="menu">
-        <Link to="/">Home</Link>
-        <Link to="/item">Product</Link>
+    <div className="app">
+      <div className="ipod">
+        <Router>
+          <div className="screen">
+            <Switch>
+              <Route path="/" exact component={Index} />
+              <Route path="/item/:route" component={Product} />
+            </Switch>
+          </div>
+          <div className="controls">
+            <Link to="/">Home</Link>
+            <Link to="/item">Product</Link>
+            <Link to="/item">Product</Link>
+            <Link to="/item">Product</Link>
+          </div>
+        </Router>
       </div>
-    </Router>
+      
+    </div>
   );
 }
 
