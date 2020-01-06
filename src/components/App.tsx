e03ed9981cb43fba9988593863cd8aa3b8a3fcca
Enrique Chong <echong112@gmail.com>
2020-01-06 12:27:30 -0500

    save point

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,30 +4,39 @@ import Index from './Index';
 import Experience from './Experience';
 import Education from './Education';
 import Portfolio from './Portfolio';
+import Wheel from './controls/Wheel';
+
 import './App.scss';
 
 const App: React.FC = () => {
   return (
     <div className="app">
-      <div className="ipod">
-        <Router>
+      <Router>
+        <div className="ipod">
+          
           <div className="bezel">
-            <Switch>
-              <Route path="/" exact component={Index} />
-              <Route path="/experience" component={Experience} />
-              <Route path="/education" component={Education} />
-              <Route path="/portfolio" component={Portfolio} />
-            </Switch>
+            <div className="screen">
+              <Switch>
+                <Route path="/" exact component={Index} />
+                <Route path="/experience" component={Experience} />
+                <Route path="/education" component={Education} />
+                <Route path="/portfolio" component={Portfolio} />
+              </Switch>
+            </div>
           </div>
+
           <div className="controls">
-            <Link to="/">Home</Link>
-            <Link to="/education">Education</Link>
-            <Link to="/experience">Experience</Link>
-            <Link to="/portfolio">Portfolio</Link>
+            <div className="buttons">
+              <Link to="/">Home</Link>
+              <Link to="/education">Education</Link>
+              <Link to="/experience">Experience</Link>
+              <Link to="/portfolio">Portfolio</Link>
+            </div>
+            <Wheel />
           </div>
-          <div className="wheel"></div>
-        </Router>
-      </div>
+
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/controls/Wheel.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Wheel: React.FC = () => {
+  return <h1>Wheel</h1>
+}
+
+export default Wheel;
