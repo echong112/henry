38ad75eff049ac215489af541e306f7954798174
Enrique Chong <echong112@gmail.com>
2020-01-07 02:06:21 -0500

    resetting

--- src/components/ClickWheel.tsx
+++ src/components/ClickWheel.tsx
@@ -8,13 +8,9 @@ const Wheel: React.FC = () => {
   const handleScroll = (event: any) => {
     dispatch(event.nativeEvent.wheelDelta > 0 ? scrollUp() : scrollDown());
   };
-
-  const handleClick = (event: any) => {
-    console.log('diahre');
-  }
+  
   return (
     <div className="wheel"
-      onClick={handleClick}
       onWheel={handleScroll}>
     </div>
   );
