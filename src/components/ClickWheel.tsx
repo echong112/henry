3e8acd5e9ac9fbcee9791c08209a733d31ffce6c
Enrique Chong <echong112@gmail.com>
2020-01-07 01:24:23 -0500

    resetting

--- src/components/ClickWheel.tsx
+++ src/components/ClickWheel.tsx
@@ -4,9 +4,11 @@ import { scrollUp, scrollDown } from '../actions';
 
 const Wheel: React.FC = () => {
   const dispatch = useDispatch();
+
   const handleScroll = (event: any) => {
     dispatch(event.nativeEvent.wheelDelta > 0 ? scrollUp() : scrollDown());
   };
+
   const handleClick = (event: any) => {
     console.log('diahre');
   }
--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -1,24 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ListItem from './ListItem';
 import { useSelector } from 'react-redux';
+
 interface Props {
   list: any[];
   parent?: string;
 };
 
 const ListView: React.FC<Props> = (props) => {
+  const [index, setIndex] = useState(0);
+
   const activeIndex = useSelector((state: any) => state.activeIndex);
 
   useEffect(() => {
-
-  });
+    setIndex(activeIndex);
+  }, [index, activeIndex]);
   
   return (
     <div>
       {props.list && props.list.map((item, k) => (
         <ListItem
           key={k}
-          isActive={k === activeIndex}
+          isActive={k === index}
           title={item.title}
           slug={item.slug}
         />
