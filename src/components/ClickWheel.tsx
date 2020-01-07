0973f70f32baa5eca81a8f36747916d1c29fb141
Enrique Chong <echong112@gmail.com>
2020-01-07 17:16:45 -0500

    menu button

--- src/components/ClickWheel.tsx
+++ src/components/ClickWheel.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, useHistory} from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { clickWheel, scrollUp, scrollDown } from '../actions';
 const Wheel: React.FC = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   return (
     <div className="controls">
       <div className="wheel" onWheel={(event: any) => dispatch(event.nativeEvent.wheelDelta > 0 ? scrollUp() : scrollDown())}>
         <div className="clicker" onClick={(e: any) => dispatch(clickWheel())}></div>
         <div className='square'>
           <svg viewBox='0 0 100 100'>
-            <p><polygon points='5,5 50,50 95,5' /></p>
+            <a onClick={() => history.goBack()}><polygon points='5,5 50,50 95,5' /></a>
             <polygon points='5,5 50,50 5,95' />
             <polygon points='5,95 50,50 95,95' />
             <polygon points='95,5 50,50 95,95' />
