ad2d224556dd16c96dc75b2dd97f24beae67d179
Enrique Chong <echong112@gmail.com>
2020-01-08 15:23:04 -0500

    moving app tocomponents

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
-import App from './routes/App/App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 const store = createStore(rootReducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
