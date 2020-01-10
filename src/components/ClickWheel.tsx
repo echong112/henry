2c9b142f65008ea43bac792a3f5060be9ac53288
Enrique Chong <echong112@gmail.com>
2020-01-09 21:22:25 -0500

    refactoringClickWheel

--- src/components/ClickWheel.tsx
+++ src/components/ClickWheel.tsx
@@ -5,13 +5,18 @@ import { clickWheel, scrollUp, scrollDown } from '../actions';
 const Wheel: React.FC = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const menuClicked = () => history.goBack();
+  const handleWheelClick = () => dispatch(clickWheel());
+  const handleScroll = (e: any) => dispatch(e.nativeEvent.wheelDelta > 0 ? scrollUp() : scrollDown())
+
   return (
     <div className="controls">
-      <div className="wheel" onWheel={(event: any) => dispatch(event.nativeEvent.wheelDelta > 0 ? scrollUp() : scrollDown())}>
-        <div className="clicker" onClick={(e: any) => dispatch(clickWheel())}></div>
+      <div className="wheel" onWheel={handleScroll}>
+        <div className="clicker" onClick={handleWheelClick}></div>
         <div className='square'>
           <svg viewBox='0 0 100 100'>
-            <a onClick={() => history.goBack()}>
+            <a onClick={menuClicked}>
               <polygon points='0,0 50,50 95,0' />
             </a>
             <polygon points='0,0 50,50 0,95' />
