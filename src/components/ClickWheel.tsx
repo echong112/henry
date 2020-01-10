098c68a1d90c76c26c6306d2f7794597021c5667
Enrique Chong <echong112@gmail.com>
2020-01-09 21:13:04 -0500

    polygon fill

--- src/components/ClickWheel.tsx
+++ src/components/ClickWheel.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import { useHistory} from "react-router-dom";
 import { useDispatch } from 'react-redux';
-import { clickWheel, scrollUp, scrollDown, clickMenuButton } from '../actions';
+import { clickWheel, scrollUp, scrollDown } from '../actions';
 const Wheel: React.FC = () => {
   const dispatch = useDispatch();
-
+  const history = useHistory();
   return (
     <div className="controls">
-      <div className="wheel" onWheel={
-        // clean this up by making it single variable. single action with param
-        (e: any) => dispatch(e.nativeEvent.wheelDelta > 0 ? scrollUp() : scrollDown())
-      }>
-        <div className="clicker" onClick={() => dispatch(clickWheel())}></div>
+      <div className="wheel" onWheel={(event: any) => dispatch(event.nativeEvent.wheelDelta > 0 ? scrollUp() : scrollDown())}>
+        <div className="clicker" onClick={(e: any) => dispatch(clickWheel())}></div>
         <div className='square'>
           <svg viewBox='0 0 100 100'>
-            <polygon onClick={() => dispatch(clickMenuButton())} points='5,5 50,50 95,5' />
-            <polygon onClick={() => dispatch(clickMenuButton())} points='5,5 50,50 95,5' />
-            <polygon onClick={() => dispatch(clickMenuButton())} points='5,5 50,50 95,5' />
-            <polygon onClick={() => dispatch(clickMenuButton())} points='5,5 50,50 95,5' />
+            <a onClick={() => history.goBack()}>
+              <polygon points='0,0 50,50 95,0' />
+            </a>
+            <polygon points='0,0 50,50 0,95' />
+            <polygon points='5,100 50,50 95,100' />
+            <polygon points='100,5 50,50 100,95' />
           </svg>
         </div>
       </div>
