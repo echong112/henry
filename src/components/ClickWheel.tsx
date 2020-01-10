d31d37537dd7cd718587963f383221ed40cf0c8a
Enrique Chong <echong112@gmail.com>
2020-01-09 22:04:10 -0500

    keyboard shortcuts

--- src/components/ClickWheel.tsx
+++ src/components/ClickWheel.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { clickMenuButton, clickWheel, scrolled } from '../actions';
 
 const Wheel: React.FC = () => {
-  // Redux Functions
   const dispatch = useDispatch();
   const menuClicked = () => dispatch(clickMenuButton())
   const handleWheelClick = () => dispatch(clickWheel());
   const handleScroll = (e: any) => dispatch(scrolled(e.nativeEvent.wheelDelta));
+
+  const keyboardFunction = useCallback((event: any) => {
+    switch(event.keyCode) {
+      case 38 : handleScroll({nativeEvent: { wheelDelta: 1}}); break;// up = scroll up
+      case 40 : handleScroll({nativeEvent: { wheelDelta: -1}}); break;// down = scroll down
+      case 37 : menuClicked(); break;// left = menu
+      case 39 : handleWheelClick(); break; // right = click
+    }
+  }, []);
+  
+  useEffect(() => {
+    document.addEventListener("keydown", keyboardFunction, false);
+    return () => {
+      document.removeEventListener("keydown", keyboardFunction, false);
+    };
+  }, []);
 
   return (
     <div className="controls">
