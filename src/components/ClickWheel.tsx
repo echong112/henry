8bcedc9b18ba022f26b9de4aef937d29dfcc470b
Enrique Chong <echong112@gmail.com>
2020-01-12 15:55:01 -0500

    saving sound. onto touch events;

--- src/components/ClickWheel.tsx
+++ src/components/ClickWheel.tsx
@@ -1,28 +1,30 @@
 import React, { useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-
 import { clickMenuButton, clickWheel, scrolled, unsetIsBack } from '../actions';
 
 const Wheel: React.FC = () => {
   const dispatch = useDispatch();
+  const mp3 = `${window.location.href}/ipodclick.mp3`;
+  let scrollWheel: any;
+
   const menuClicked = useCallback(() => {
-    let a = new Audio('http://localhost:3000/ipodclick.mp3');
-    a.play();
+    const click = new Audio(mp3);
+    click.play();
     dispatch(clickMenuButton());
-  }, [dispatch]);
+  }, [dispatch, mp3]);
 
   const handleWheelClick = useCallback(() => {
-    let a = new Audio('http://localhost:3000/ipodclick.mp3');
-    a.play();
+    const click = new Audio(mp3);
+    click.play();
     dispatch(unsetIsBack());
     dispatch(clickWheel());
-  }, [dispatch]);
+  }, [dispatch, mp3]);
 
   const handleScroll = useCallback((e: any) => {
-    let a = new Audio('http://localhost:3000/ipodclick.mp3');
-    a.play();
+    const click = new Audio(mp3);
+    click.play();
     dispatch(scrolled(e.nativeEvent.wheelDelta));
-  }, [dispatch]);
+  }, [dispatch, mp3]);
 
   const upKey = useCallback((event: any) => {
     if (event.keyCode === 38) handleScroll({nativeEvent: { wheelDelta: 1}}); // up
@@ -30,8 +32,20 @@ const Wheel: React.FC = () => {
     if (event.keyCode === 37) menuClicked(); // left
     if (event.keyCode === 39) handleWheelClick(); // right
   }, [handleScroll, handleWheelClick, menuClicked]);
+
+  const process_touchstart = (e: any) => {
+    console.log(e);
+    console.log(e.touches[0].pageX);
+  }
+  const process_touchend = () => {
+    console.log('touch end');
+  }
   
   useEffect(() => {
+    console.log(scrollWheel);
+    // determine clockwise or counter clockwise
+    scrollWheel.addEventListener('touchmove', process_touchstart, false);
+    scrollWheel.addEventListener('touchend', process_touchend, false);
     document.addEventListener("keydown", upKey, false);
     return () => {
       document.removeEventListener("keydown", upKey, false);
@@ -40,7 +54,7 @@ const Wheel: React.FC = () => {
 
   return (
     <div className="controls">
-      <div className="wheel" onWheel={handleScroll}>
+      <div className="wheel" ref={input => scrollWheel = input} onWheel={handleScroll}>
         <div className="clicker" onClick={handleWheelClick}></div>
         <div className='clicker-buttons'>
           <svg viewBox='0 0 100 100'>
