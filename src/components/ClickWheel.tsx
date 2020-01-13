67301dbd0c9228f4fc0070a392021d0c45c7114a
Enrique Chong <echong112@gmail.com>
2020-01-12 22:28:38 -0500

    resetting

--- src/components/ClickWheel.tsx
+++ src/components/ClickWheel.tsx
@@ -3,10 +3,8 @@ import { useDispatch } from 'react-redux';
 import { clickMenuButton, clickWheel, scrolled, unsetIsBack } from '../actions';
 
 const Wheel: React.FC = () => {
-  const [samples, setSamples] = useState([]);
   const mp3 = `${window.location.href}/ipodclick.mp3`;
   const dispatch = useDispatch();
-  let scrollWheel: any;
 
   const menuClicked = useCallback(() => {
     const click = new Audio(mp3);
@@ -34,27 +32,22 @@ const Wheel: React.FC = () => {
     if (event.keyCode === 39) handleWheelClick(); // right
   }, [handleScroll, handleWheelClick, menuClicked]);
 
-  // determine clockwise or counter clockwise
-  const process_touchstart = useCallback((e: any) => {
-    const currSamples = Object.assign([], samples);
-  },[samples]);
-
   useEffect(() => {
-    scrollWheel.addEventListener('touchmove', process_touchstart, false);
     document.addEventListener("keydown", upKey, false);
     return () => {
       document.removeEventListener("keydown", upKey, false);
     };
-  }, [upKey, scrollWheel, process_touchstart]);
-
+  }, [upKey]);
+  
   return (
     <div className="controls">
-    {samples}
-      <div className="wheel" ref={input => scrollWheel = input} onWheel={handleScroll}>
+      <div className="wheel"
+        onWheel={handleScroll}
+      >
         <div className="clicker" onClick={handleWheelClick}></div>
         <div className='clicker-buttons'>
           <svg viewBox='0 0 100 100'>
-            <polygon onClick={menuClicked} points='0,0 50,50 100,0' />
+            <polygon points='0,0 50,50 100,0' onClick={menuClicked} />
             <polygon points='0,0 50,50 0,100' />
             <polygon points='0,100 50,50 100,100' />
             <polygon points='100,0 50,50 100,100' />
