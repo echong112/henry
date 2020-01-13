2455e73e937cc00348f424a2650575889d1440a0
Enrique Chong <echong112@gmail.com>
2020-01-13 01:02:54 -0500

    adding scroll control for page

--- src/routes/Skills.tsx
+++ src/routes/Skills.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { skills } from './_routes';
 
-const Skills: React.FC = () => (
-  <div className="page">
-    <h3>Skills</h3>
-    {skills && skills.map((skill, i) => (
-      <div>
-        <p>{skill.title}</p>
-        {skill.skills.map((item, j) => {
-          return (
-            <p>{item}</p>
+const Skills: React.FC = () => {
+  const activeIndex = useSelector((state: any) => state.activeIndex);
+  let thisPage: any;
+  useEffect(() => {
+    if (activeIndex && thisPage) {
+      console.log(window);
+      thisPage.scrollTo(activeIndex, activeIndex * 10)
+      console.log(activeIndex);
+    }
+  }, [activeIndex]);
+
+  return (
+    <div className="page" ref={myPage => thisPage = myPage}>
+      <h3>Skills</h3>
+      {skills && skills.map((skill, i) => (
+        <div key={i}>
+          <h4>{skill.title}</h4>
+          {skill.skills.map((item, j) => <p key={j}>{item}</p>)}
+        </div>
           )
-        })}
-      </div>
-        )
-    )}
-    <p></p>
-  </div>
-);
+      )}
+      <p></p>
+    </div>
+  )
+}
 
 export default Skills;
