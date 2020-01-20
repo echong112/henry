5b4f8ff1a0bac905c336d39642838c63f9fef8ec
Enrique Chong <echong112@gmail.com>
2020-01-20 15:29:08 -0500

    moving logic to list

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
-// eslint-disable-next-line
-import { Link, useHistory} from "react-router-dom";
-import { useSelector, useDispatch} from 'react-redux';
-import { playMedia, unClick, savePlace } from '../actions';
+import React, { useState } from 'react';
+import { Link} from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 interface Props {
   currItem: any;
@@ -12,31 +10,9 @@ interface Props {
 };
 
 const MenuItem: React.FC<Props> = (props) => {
-  const [url, setUrl] = useState('/');
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const clicked = useSelector((state: any) => state.clicked);
+  const [url] = useState('/');
   const isPlaying = useSelector((state: any) => state.isPlaying);
 
-  useEffect(() => {
-    let link = '/';
-    let parent = props.currItem.parent;
-    let slug = props.currItem.slug;
-
-    link = parent ? `${parent}/${slug}` : slug
-
-    setUrl(`/${link}`);
-
-    if (clicked && props.isActive) {
-      dispatch(unClick());
-      dispatch(savePlace(props.itemKey));
-      if (slug === 'player') {
-        dispatch(playMedia());
-      } else {
-        history.push(link);
-      }
-    }
-  }, [clicked, dispatch, history, props]);
   return (
     <Link className={`menu-item ${props.isActive ? 'active' : ''} ${props.slug === 'nowplaying' ? 'now-playing' : ''} ${isPlaying ? 'playing' : ''}`} to={{
       pathname: url,
--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { resetScroll, setPlace } from '../actions';
+import { Link, useHistory} from "react-router-dom";
+import { resetScroll, setPlace, playMedia, unClick, savePlace } from '../actions';
 import ListItem from './ListItem';
 
 interface Props {
@@ -9,11 +10,32 @@ interface Props {
 };
 
 const ListView: React.FC<Props> = (props) => {
+  const [url, setUrl] = useState('/');
   const dispatch = useDispatch();
+  const history = useHistory();
   const [index, setIndex] = useState(0);
   const activeIndex = useSelector((state: any) => state.activeIndex);
   const savedIndexes = useSelector((state: any) => state.savedIndexes);
   const isBack = useSelector((state: any) => state.isBack);
+  const clicked = useSelector((state: any) => state.clicked);
+
+  useEffect(() => {
+    if (clicked) {
+      let parent = props.list[index].parent;
+      let slug = props.list[index].slug;
+      let link = parent ? `${parent}/${slug}` : slug;
+  
+      setUrl(`/${link}`);
+  
+      dispatch(unClick());
+      dispatch(savePlace(index));
+      if (slug === 'player') {
+        dispatch(playMedia());
+      } else {
+        history.push(link);
+      }
+    }
+  }, [clicked])
 
   useEffect(() => {
     let current = index;
